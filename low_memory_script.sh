2aa9a11dc07f0205b4bd96597e1010484c4ebddd
Ubuntu <ubuntu@ip-172-31-14-229.ap-south-1.compute.internal>
2023-09-26 10:00:43 +0000

    disk space checking script added

---
+++ low_memory_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+df -h | awk '{print $5 " " $1}' | while read output;
+do
+	#echo "Disk details :  $output"
+        usage=$(echo $output | awk '{print $1}' | cut -d'%' -f1)
+	file_sys=$(echo $output | awk '{print $2}') 
+	#echo $usage
+	if [ $usage -le 20 ]
+	then
+		echo "CRITICAL !! for : $file_sys  = $usage" 
+	fi
+
+
+done
